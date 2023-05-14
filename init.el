19b97feac432b003c90aad517112c22ba80e3ec5
roum5 <maximerousseau08@gmail.com>
2023-05-13 21:21:23 -0400

    initel

---
+++ init.el
@@ -0,0 +1,45 @@
+;; Windows init location (c:\Users\roum5\AppData\Roaming\.emacs.d\init.el)
+;; Linux init location (~/.emacs.d/init.el)
+;;(load-file "c:/Users/roum5/source/dotfiles/emacs/config.el")
+(load-file "c:/Users/roum5/source/mr-emacs/base.el")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "ed8e6f452855fc7338c8be77803666b34745c19c6667197db48952107fa6d983" "c4b0cd42365d27c859dc944197c9e5c36ff0725c66eb03fdf21dc8b004566388" "a9e05a597a1fa5b7b9ca70accdcb0d173530a04d15175d8a1674b7f4ad5a59ba" "2043768e4db5313db5d30fc4b3e6828cda3b775180d24ff599e65cc55b9db898" "53d8beef99d453a99a5f491c6bcf4954f1af986ad9160868d80ce8f4e8e30cd0" "9eb87e3b725057ccdc248efaac3421eca4c758b84a397ad829d505ef77fe94c5" default))
+ '(fci-rule-color "#778ca3")
+ '(nrepl-message-colors
+   '("#00afef" "#778ca3" "#009c9f" "#778ca3" "#005cc5" "#fa1090" "#009c9f" "#778ca3"))
+ '(package-selected-packages
+   '(all-the-icons-dired dired-sidebar all-the-icons flycheck good-scroll nyan-mode avy god-mode hydra company-emojify emojify doom-themes cython-mode moody yasnippet-snippets yasnippet yasnippets org-tree-slide org-superstar solo-jazz-theme blacken virtualenvwrapper eglot counsel swiper evil-org evil-leader evil use-package darkroom company ivy))
+ '(pdf-view-midnight-colors '("#778ca3" . "#eaeafa"))
+ '(vc-annotate-background "#04c4c7")
+ '(vc-annotate-color-map
+   '((20 . "#778ca3")
+     (40 . "#00afef")
+     (60 . "#778ca3")
+     (80 . "#778ca3")
+     (100 . "#778ca3")
+     (120 . "#009c9f")
+     (140 . "#778ca3")
+     (160 . "#778ca3")
+     (180 . "#778ca3")
+     (200 . "#778ca3")
+     (220 . "#009c9f")
+     (240 . "#005cc5")
+     (260 . "#fa1090")
+     (280 . "#778ca3")
+     (300 . "#005cc5")
+     (320 . "#778ca3")
+     (340 . "#009c9f")
+     (360 . "#778ca3")))
+ '(vc-annotate-very-old-color "#778ca3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(god-mode-lighter ((t (:inherit error)))))
